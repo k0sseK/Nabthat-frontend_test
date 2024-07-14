0d57b88c6c37e90e13bc5407b19396d9fa536cc6
k0sseK <k0ssekcs@gmail.com>
2024-07-14 17:19:58 +0200

    feat: Add App.vue component with header, main, and footer sections

--- task-solution/src/main.ts
+++ task-solution/src/main.ts
@@ -1,6 +1,11 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import './assets/styles/main.scss'
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
